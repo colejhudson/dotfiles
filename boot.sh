345ace0bb17040f7d76e0c1e63a6f6941e5bc580
Cole Hudson <colehudson1997@gmail.com>
2018-07-19 15:13:28 -0700

    Fleshed out bootscript completely

--- boot.sh
+++ boot.sh
@@ -9,14 +9,14 @@ export USER_SRC=${USER_ROOT}/src # account specific source directory
 export USER_ETC=${USER_ROOT}/etc # account specific configuration files
 export USER_DOT=${USER_ROOT}/dotfiles 
 
-[[ ! (-e ${SYSCONFIGPATH}) ]] && {
+[[ -e ${SYSCONFIGPATH} ]] || {
   sudo -c "
     mkdir ${SYSCONFIGPATH};
     curl https://gitlab.com/colejhudson/dotfiles/raw/master/.bash_profile > ${SYSCONFIGPATH}/.bash_profile; \
   "
 }
 
-[[ ! (-e ${USER_ROOT}) ]] && {
+[[ -e ${USER_ROOT} ]] || {
   mkdir ${USER_ROOT}
   mkdir ${USER_SRC}
   mkdir ${USER_ETC}
@@ -27,11 +27,20 @@ export USER_DOT=${USER_ROOT}/dotfiles
   cp ${SYSCONFIGPATH}/.bash_profile ${USER_DOT}/.bash_profile
   ln -s  ${USER_DOT}/.bash_profile ${HOME}/.bash_profile
 
-  curl "https://gitlab.com/colejhudson/dotfiles/blob/master/$(basename ${SCRIPT})/.$(uname | tr [[:upper:]] [[:lower:]])" > ${USER_DOT}/$(basename ${SCRIPT})/.$(uname | tr [[:upper:]] [[:lower:]])
+  (
+    read -p "Which config would you like to install? (e.g. bash, fish, csh):" SHELL
+    case ${SHELL} in
+      bash|Bash)
+        curl "https://gitlab.com/colejhudson/dotfiles/blob/master/$(basename ${SCRIPT})/.$(uname | tr [[:upper:]] [[:lower:]])" 
+      ;;
+      *)
+        echo "A configuration file for that shell isn't available. Exiting."
+        return 1
+      ;;
+    esac 
+  )> ${USER_DOT}/$(basename ${SCRIPT})/.$(uname | tr [[:upper:]] [[:lower:]])
 }
 
 source ${HOME}/.bash_profile
-# TODO: Add default commands to install
 
-# Install Homebrew
-# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+command -v brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
