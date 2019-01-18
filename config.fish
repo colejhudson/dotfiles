2ff0e0c331a85c1328c9dc27e7bd6edf611658d0
Cole Hudson <cole@colejhudson.com>
2019-01-18 00:50:55 -0800

    added fish configuration

---
+++ config.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+
+set ROOT $HOME/.(whoami)
+set SRC $ROOT/src
+set ETC $ROOT/etc
+set DOT $ROOT/dotfiles
+
+source $DOT/.variables
+source $DOT/.functions
+source $DOT/.aliases
+source $DOT/.config
+source $DOT/.env
