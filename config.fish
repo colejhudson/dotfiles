9bac93090bf3daed4af8fce75a735f72a5c1e7cc
Cole Hudson <cole@colejhudson.com>
2019-01-26 20:54:41 -0800

    organized user and system setup

--- config.fish
+++ config.fish
@@ -5,13 +5,13 @@ set SRC $ROOT/src
 set ETC $ROOT/etc
 set DOT $ROOT/dotfiles
 
+if test $CONFIGURED; and status --is-interactive
+	source $DOT/.(uname -s | tr [[:upper:]] [[:lower:]])
+	set -U CONFIGURED true
+end
+
 source $DOT/.variables
 source $DOT/.functions
 source $DOT/.aliases
 source $DOT/.config
 source $DOT/.env
-
-if test $CONFIGURED; and status --is-interactive
-	source $DOT/.(uname -s)
-	set -U CONFIGURED true
-end
